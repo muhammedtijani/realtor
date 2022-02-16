645d5db472481a293b4aec8b41c4a2cad9b5f53c
Muhammed Shittu <coolzyte@gmail.com>
2022-02-16 07:38:36 +0100

    project completed

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -13,7 +13,7 @@ import { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
 
-// import { filterData, getFilterValues } from "../utils/filterData";
+import { filterData, getFilterValues } from "../utils/filterData";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 import noresult from "../assets/images/noresult.svg";
 
